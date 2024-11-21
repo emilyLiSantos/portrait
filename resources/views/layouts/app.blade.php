0cb1596dc50281fc80ced186eb62bab271ec3914
lucas leles <lucaslelisdossantos@hotmail.com.br>
2024-11-20 23:14:33 -0300

    Fazendo o a pagina, nao fiz a parte do backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,19 +6,81 @@
 -->
 <html>
 	<head>
-		<title>Verti by HTML5 UP</title>
+		<title>Portrait</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="{{  url('assets/css/main.css')  }}" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	</head>
-	<body class="is-preload homepage" style="background-color:rgb(0, 0, 0)">
+<style>
+            /* testando segunda pagina*/
+            .page {
+                background-image: url(https://media.istockphoto.com/id/1853173882/pt/foto/large-scarlet-macaw-couple-flying-inside-buraco-das-araras-in-the-city-of-jardim-in-the.jpg?s=2048x2048&w=is&k=20&c=asdQEK5t8lBvemBV2oslDJiE-Sn9MelGBSKs7gRmx2c=);
+                background-size: cover;
+                background-position: center;
+                position: relative;
+                height: 85vh;
+            }
+
+
+            .custom-section {
+                background-color: #eaf9f027;
+                padding: 50px 0;
+            }
+            .custom-btn {
+                background-color: #8f0015;
+                border: none;
+            }
+            /*.custom-btn:hover {
+                background-color: #ff0a2a;
+            }*/
+        </style>
+
+<style>
+    /* Pagina 3*/
+    .background-imagen {
+        background-image: url('https://cdn.pixabay.com/photo/2023/08/23/15/44/ai-generated-8208847_1280.png');
+        background-size: cover;
+        background-position: center;
+        position: relative;
+        height: 100vh;
+    }
+
+    /* texto de fundo da pagina  */
+    .background-imagen .text-content {
+        position: absolute;
+        top: 30%;
+        left: 5%;
+        color: white;
+        z-index: 2;
+    }
+
+    .background-imagen .text-content h1 {
+        font-size: 3rem;
+        font-weight: bold;
+    }
+
+    /* Forms */
+    .form-container {
+        position: absolute;
+        top: 20%;
+        right: 5%;
+        background-color: white;
+        padding: 30px;
+        border-radius: 10px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+        z-index: 3;
+        max-width: 400px;
+    }
+</style>
+
+    </head>
+	<body class="is-preload homepage">
 		<div id="page-wrapper">
 
-			<!-- Header -->
-			<header>
-               
-                <nav class="navbar navbar-expand-lg" style="background-color:black; padding-top: 30px;">
+
+
+
+               <!-- <nav class="navbar navbar-expand-lg" style="background-color:black; padding-top: 30px;">
                     <div class="container-fluid">
                       <a class="navbar-brand" href="#">
                         <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR7qvJYXxJFO5Y8j-O5XBPLaLjdaqx_Fe1SNfSwnYzI-N6ouVS9" alt="logo" width="65" height="60">
@@ -43,10 +105,31 @@
                     </div>
 
                     </div>
-                </nav>
-			</header>
+                </nav>-->
 
-			<!-- Main -->
+         <!--Nabar-->
+                <nav class="navbar navbar-expand-lg border-bottom" style="background-color:black; padding-top: 30px;">
+                    <div class="container-fluid" >
+                        <a class="navbar-brand" href="#">
+                            <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR7qvJYXxJFO5Y8j-O5XBPLaLjdaqx_Fe1SNfSwnYzI-N6ouVS9" width="45px" alt="">
+                            <span style="color:#eaf9f0;">Portrait</span>
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item"><a class="nav-link" href="#" style="color:#fff">Sobre</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#" style="color:#fff">Explorar</a></li>
+                                <li class="nav-item"><a class="btn btn-danger ms-2" href="#" style="color:#fff">Entrar</a></li>
+                                <li class="nav-item"><a class="btn btn-danger ms-2" href="#" style="color:#fff">Criar Conta</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+              <!--fim navbar-->
+
+			<!-- Main/welcome-->
 				<div id="main-wrapper" style="margin-top: 0%">
 					<div class="">
 						<div class="">
@@ -60,9 +143,10 @@
 						</div>
 					</div>
 				</div>
+             <!--fim Main-->
 
-			<!-- Footer -->
-            <footer class="rodape mt-5 mb-3">
+             <!-- Footer -->
+            <footer class="rodape mt-5 mb-3" style="background-color:black; padding-top: 30px;">
                 <div class="container text-center">
                     <div class="row p-5">
                         <div class="col-md-3 mt-3">
@@ -93,7 +177,7 @@
                     </div>
 
             </footer>
-
+     <!--fim Footer-->
 		</div>
 
 		<!-- Scripts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<section class="carrossel1">
+<!--<section class="carrossel1">
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
@@ -23,45 +23,38 @@
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
-      </div>
+      </div>-->
 </section>
-
-
-
-<!-- Features -->
-    <div id="features-wrapper"  style="background-color:rgba(52, 54, 156, 0.548); border-radius: 25px;" >
+   <!--Features
+  <div id="features-wrapper "  style="background-color:rgba(52, 54, 156, 0.548); border-radius: 25px;">
         <div class="container">
             <div class="row">
                 <div class="col-4 col-12-medium">
 
-                    <!-- Box -->
-                        <section class="">
+                    Box
+                        <section style="text-align: center; padding:35px;">
                             <a href="#" class="image featured"><img src="" alt="" /></a>
                             <div class="inner">
                                 <header>
-                                    <h2 style="font-size: 28px;">Put something here</h2>
-                                    <p>Maybe here as well I think</p>
+                                    <h2 style="font-size: 48px;">Cadastre-se</h2>
+                                    <p style="font-size: 30px;">tenha novas ideias</p>
                                 </header>
 
                             </div>
                         </section>
 
-                </div>
+                </div>-->
 
-                <div class="col-4 col-12-medium">
+                 <!-- <div class="col-4 col-12-medium">
 
-                    <!-- Box-->
-                        <section class="box feature">
-                            <a href="#" class="image featured"><img src="https://services.meteored.com/img/article/inteligencia-artificial-aprende-a-reconstruir-imagens-vistas-por-pessoas-ciencia-fotos-1679175318563_1280.jpg" alt="" /></a>
-                            <div class="inner">
-                            </div>
-                        </section>
+                   Box
+
 
                 </div>
 
-                <div class="col-4 col-12-medium">
+                <div class="col-4 col-12-medium">-->
 
-                    <!-- Formulario-->
+                    <!-- Formulario
                         <section class="">
                             <form>
                                 <div class="mb-3">
@@ -77,54 +70,93 @@
                                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary">Cadastrar</button>
                               </form>
-                        </section>
+                        </section>-->
 
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- Main -->
-    <div id="main-wrapper">
+     <!--Pagina 1 -->
+    <div class="page">
         <div class="container">
-            <div class="row gtr-200">
-                <div class="col-4 col-12-medium">
-
-                    <!-- Sidebar -->
-                        <div id="sidebar">
-                            <section class="widget thumbnails">
-                                <h3>Interesting stuff</h3>
-                                <div class="grid">
-                                    <div class="row gtr-50">
-                                        <div class="col-6"><a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a></div>
-                                        <div class="col-6"><a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a></div>
-                                        <div class="col-6"><a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a></div>
-                                        <div class="col-6"><a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a></div>
-                                    </div>
-                                </div>
-                                <a href="#" class="button icon fa-file-alt">More</a>
-                            </section>
-                        </div>
-
-                </div>
-                <div class="col-8 col-12-medium imp-medium">
-
-                    <!-- Content -->
-                        <div id="content">
-                            <section class="last">
-                                <h2>So what's this all about?</h2>
-                                <p>This is <strong>Verti</strong>, a free and fully responsive HTML5 site template by <a href="http://html5up.net">HTML5 UP</a>.
-                                Verti is released under the <a href="http://html5up.net/license">Creative Commons Attribution license</a>, so feel free to use it for any personal or commercial project you might have going on (just don't forget to credit us for the design!)</p>
-                                <p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper bibendum ipsum, et tristique augue fringilla eu. Vivamus id risus vel dolor auctor euismod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat. Aliquam luctus mattis lectus sit amet phasellus quam turpis.</p>
-                                <a href="#" class="button icon solid fa-arrow-circle-right">Continue Reading</a>
-                            </section>
-                        </div>
+            <div class="row align-items-center ">
+                <!-- Imagem
+                <div class="col-md-6 text-center">
+                    <img src=""  class="img-fluid rounded" alt="Imagem">
+                </div>-->
+                <!-- Coluna do texto -->
+                <div class=" justify-content-center align-items-center text-center" style="text-align: center;">
+                    <h1 class="display-4 fw-bold text-white">Bem vindo ao Portrait</h1>
+                    <p class="fs-5 text-white">
+                        A melhor parte do Portrait.
+                    </p>
 
                 </div>
             </div>
         </div>
     </div>
-</div>/*
+
+
+
+    <!--pagina 2-->
+    <div class="custom-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Imagem -->
+                <div class="col-md-6 text-center">
+                    <img src="https://cdn.pixabay.com/photo/2023/01/19/10/24/phone-booth-7728783_1280.jpg"  class="img-fluid rounded" alt="Imagem">
+                </div>
+                <!-- Coluna do texto -->
+                <div class="col-md-6">
+                    <h1 class="fw-bold text-danger">Use o explorar </h1>
+                    <p class="fs-5 text-muted">
+                        A melhor parte do Portrait.
+                    </p>
+                   <a class="nav-item"><a class="btn btn-danger ms-2" href="#" style="color:#fff">Explorar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!--Pagina 3 com forms, OBs:nao sei como coloca o login direto com o google-->
+    <div class="background-imagen">
+        <!-- Texto sobre -->
+        <div class="text-content">
+            <h1><br></h1>
+        </div>
+
+        <!-- Formulário -->
+        <div class="form-container">
+            <h2 class="text-center">Bem-vindo(a) ao Portrait</h2>
+            <p class="text-center">Crie sua conta</p>
+
+            <!--forms-->
+            <form>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Senha</label>
+                    <input type="password" class="form-control" id="password" placeholder="Crie uma senha">
+                </div>
+                <div class="mb-3">
+                    <label for="dob" class="form-label">Data de nascimento</label>
+                    <input type="date" class="form-control" id="dob">
+                </div>
+                <button type="submit" class="btn btn-danger w-100">Continuar</button>
+                <div class="text-center mt-3">
+                    <p>ou</p>
+                    <button type="button" class="btn btn-outline-secondary w-100">Continuar com o Google</button>
+                </div>
+                <p class="mt-3 text-center text-muted" style="font-size: 0.9rem;">
+                    Ao continuar, você concorda com os Termos de Serviço e confirma que leu nossa Política de Privacidade.
+                </p>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
